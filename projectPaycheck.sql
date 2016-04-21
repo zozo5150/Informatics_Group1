3d5165a0e5c8c38b677f7e26b72de8ef4f6af250
ddbingham <deandre-bingham@uiowa.edu>
2016-04-21 09:37:37 -0500

    Update projectPaycheck.sql

--- projectPaycheck.sql
+++ projectPaycheck.sql
@@ -18,6 +18,6 @@ CREATE TABLE Paycheck
 
 -- Add some sample data
 
-INSERT INTO Paycheck(PaycheckID, JobID, PaycheckHours, PayStart, PayEnd, AmountPaid) VALUES ('Buffon', 'Gianluigi', 1, 'Italy',);
+INSERT INTO Paycheck(PaycheckID, JobID, PaycheckHours, PayStart, PayEnd, AmountPaid) VALUES ('Buffon', 'Gianluigi', 1, 'Italy');
 INSERT INTO Paycheck(PaycheckID, JobID, PaycheckHours, PayStart, PayEnd, AmountPaid) VALUES ('Buffon', 'Gianluigi', 2, 'Italy');
 INSERT INTO Paycheck(PaycheckID, JobID, PaycheckHours, PayStart, PayEnd, AmountPaid) VALUES ('Buffon', 'Gianluigi', 3, 'Italy');
