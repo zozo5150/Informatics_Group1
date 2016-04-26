aab9129f276abc59d90f1f28d414734e6e66f524
Michael Crabtree <michael-crabtree@uiowa.edu>
2016-04-26 03:30:04 -0500

    admin page now table

--- adminUsers.php
+++ adminUsers.php
@@ -2,13 +2,14 @@
 	//include_once('adminPermissions.php');
 	$menuHighlight=0; 
 	$headtitle="Edit Users";
-	include_once('config.php')
-	include_once('dbutils.php')
-	//include_once("adminMenu.php");
+	include_once('config.php');
+	include_once('dbutils.php');
+	include_once("adminMenu.php");
 ?>
 
 <div class="row">
 <div class="col-xs-12">
+<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
 <table class="table table-hover">
 
 <!-- Titles for table -->
@@ -36,18 +37,19 @@
 	
 	while($row = nextTuple($result)) {
 		echo "\n <tr>";
-		echo "<td>" . $row['UserID'] . "</td>";
-		echo "<td>" . $row['LastName'] . "</td>";
-		echo "<td>" . $row['FirstName'] . "</td>";
-		echo "<td>" . $row['email'] . "</td>";
-		echo "<td>" . $row['UserPerm'] . "</td>";
-		echo "<td>" . $row['FirstName'] . "</td>";
+		echo "<td><input type='text' name=UserID value='" . $row['UserID'] . "'></td>";
+		echo "<td><input type='text' name=LastName value='" . $row['LastName'] . "'></td>";
+		echo "<td><input type='text' name=FirstName value='" . $row['FirstName'] . "'></td>";
+		echo "<td><input type='text' name=email value='" . $row['email'] . "'></td>";
+		echo "<td><input type='text' name=UserPerm value='" . $row['UserPerm'] . "'></td>";
+		echo "<td><button type='submit' class='btn btn-default' name='submit'>Edit</button></td>";
 		echo "</tr>";
 	}
 ?>
 
 </tbody>
 </table>
+</form>
 </div>
 </div>
 
