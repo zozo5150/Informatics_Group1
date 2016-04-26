83414ac0d3c6d48269d5fb6eb7df2989a7547d34
Bingham <ddbingham@serv42.divms.uiowa.edu>
2016-04-26 14:10:30 -0500

    Changes to databaserevise to allow it to load

--- databaserevise.sql
+++ databaserevise.sql
@@ -1,25 +1,24 @@
-DROP TABLE IF EXISTS Users  ;
-DROP TABLE IF EXISTS Employer;
-DROP TABLE IF EXISTS Jobs ;
-DROP TABLE IF EXISTS Hours ;
 DROP TABLE IF EXISTS Paycheck;
-
+DROP TABLE IF EXISTS Hours;
+DROP TABLE IF EXISTS Jobs;
+DROP TABLE IF EXISTS Employer;
+DROP TABLE IF EXISTS Users;
 
 CREATE TABLE Users
 (
-	UserID INT NULL AUTO_INCREMENT,
+	UserID INT AUTO_INCREMENT,
 	LastName varchar(255), 
 	FirstName varchar(255),
 	email varchar(255), 
 	hashedPass varchar(255), 
-	PRIMARY KEY (UserID), 
-	UserPerm tinyint
+	UserPerm tinyint,
+	PRIMARY KEY (UserID)
 ); 
 
 
 CREATE TABLE Employer
 (
-	EmployerID INT NULL AUTO_INCREMENT, 
+	EmployerID INT AUTO_INCREMENT, 
 	employer varchar(255), 
 	employerCity varchar(255), 
 	employerState varchar(255), 
@@ -30,10 +29,10 @@ CREATE TABLE Employer
 
 -- Advice: the foreign key EmployerID isn't available yet because the
 -- EMployer table is further down this file. Reorder tables so that the
--- tables you refer to elsewhere occur first: Employee, Employer, Jobs, Hours, Paycheck, etc.
+-- tables you refer to elsewhere occur first: Employee, Employer, Jobs, Hours, aycheck, etc.
 CREATE TABLE Jobs
 (
-	JobID INT NULL AUTO_INCREMENT, 
+	JobID INT AUTO_INCREMENT, 
 	UserID INT NOT NULL,
 	EmployerID int, 
 	Wage decimal(4,2),
@@ -45,7 +44,7 @@ CREATE TABLE Jobs
 CREATE TABLE Hours
 ( 
 	UserID INT, 
-	hourID INT NULL AUTO_INCREMENT, 
+	hourID INT AUTO_INCREMENT, 
 	hoursReported decimal(4,2), 
 	workDate date, 
 	PRIMARY KEY (hourID), 
@@ -54,11 +53,11 @@ CREATE TABLE Hours
 
 CREATE TABLE Paycheck 
 (
-	PaycheckID INT NULL AUTO_INCREMENT, 
+	PaycheckID INT AUTO_INCREMENT, 
 	PaycheckHours decimal(4,2),
 	PayStart date, 
 	PayEnd date, 
 	AmountPaid decimal(5,2), 
-	PRIMARY KEY (PaycheckID),
-	FOREIGN KEY (hoursID) REFERENCES Hours(hoursID)
+	PRIMARY KEY (PaycheckID)
+--	FOREIGN KEY (hourID) REFERENCES Hours(hourID)
 ); 
