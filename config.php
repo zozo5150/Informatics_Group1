9b6636375ddd76833b6bd12d2b0f05a3e3f3e692
ddbingham <deandre-bingham@uiowa.edu>
2016-05-04 00:46:25 -0500

    Need to change data to connect to the db MySQL

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+// Site configuration.
+
+// Server information.
+$Proto = "http://";
+$Host = $_SERVER['SERVER_NAME'];
+$Base = "/~ddbingham/pb/";
+
+// Title to use in browser title bar.
+$Title = "Wage Theft Protection";
+$Name = "DeAndre";
+$Email = "deandre-bingham@uiowa.edu";
+$Logo = "3470446259_43c53168d4_o.jpg";
+
+// DB connection (on localhost, the web server).
+$dbUser = "ddbingham";
+	$dbPassword = "bmw";
+	$dbName = "db_ddbingham";
+	$dbHost = "dbdev.cs.uiowa.edu";
+	$adminEmail = "deandre-bingham@uiowa.edu";
+?>
